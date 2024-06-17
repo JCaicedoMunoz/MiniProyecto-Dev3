4a4aef590f6632a483837e4b7b7c434339d3788b
JeniferCaicedo <jenifer.caicedo0109@gmail.com>
2024-06-17 04:12:30 -0500

    Actualizacion de componentes

---
+++ src/Title.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Title() {
+  return (
+    <div className="title">
+        <h1>Stays in Finland</h1>
+        <p>12+ stays</p>
+      </div>
+  )
+}
---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <footer>
+      <p>create by <a href='#'>JeniferCaicedo</a> - devChallenges.io</p>
+    </footer>
+  )
+}
